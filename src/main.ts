ec705b753653923ccf3f09880a8318e8c0f24c6e
Shulga Maxim <shulha.m@abcloudz.com>
2026-06-12 11:17:10 +0300

    fix: update default port in app configuration and enable CORS in main application

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(',')
+      : ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
